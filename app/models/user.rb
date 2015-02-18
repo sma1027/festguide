0ceffa6ccc6747017652777fbb7cb2da0031f5b8
sofia ma <sofia.ma@gmail.com>
2015-02-18 18:01:40 -0500

    user login and logout, allowing only admin to delete user

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save { self.email = email.downcase }
+
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root 'artists#index'
 
-  resources :artists, [:only] => 'index'
+  resources :'artists', :only => ['index', 'new']
+
+  get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  resources :'users'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
