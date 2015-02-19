4cdcb4d59c1410cb646e7a0e191c139365eea47f
sofia ma <sofia.ma@gmail.com>
2015-02-19 15:42:40 -0500

    admin default value to false, created edit page for artist for admin only

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,4 +2,16 @@ class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
   end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def edit
+    if user_signed_in? && current_user.admin?
+      @artist = Artist.find(params[:id])
+    else
+      redirect_to artist_path
+    end
+  end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,7 +2,7 @@
 require 'open-uri'
 
 class Artist < ActiveRecord::Base
-  
+
   def self.create_artists
     html = Nokogiri::HTML(open('http://www.edmsauce.com/artists/'))
     html.css('h2').each do |artist| 
