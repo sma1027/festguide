10d102bffb777d7e45cc3092e1083b0c9b371989
sofia ma <sofia.ma@gmail.com>
2015-02-21 15:28:15 -0500

    added twitter_id to artist_params

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -23,6 +23,6 @@ class ArtistsController < ApplicationController
 
   private
     def artist_params
-      params.require(:artist).permit(:name, :youtube_id, :instagram_id)
+      params.require(:artist).permit(:name, :youtube_id, :instagram_id, :twitter_id)
     end
 end
