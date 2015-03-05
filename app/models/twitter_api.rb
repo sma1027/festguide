7435a1917f6eed960b2ac2995c9e5b210a43e1d4
sofia ma <sofia.ma@gmail.com>
2015-03-05 02:33:43 -0500

    added link to view in twitter

--- app/models/twitter_api.rb
+++ app/models/twitter_api.rb
@@ -2,7 +2,7 @@ class TwitterApi
   # attr_accessor :client
 
   # def initialize
-  @@client = Twitter::REST::Client.new do |config|
+  @@client ||= Twitter::REST::Client.new do |config|
       config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
       config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
       config.access_token = ENV['TWITTER_ACCESS_TOKEN']
@@ -11,13 +11,14 @@ class TwitterApi
   # end
 
   def self.user_timeline(username)
-    @@client.user_timeline(username, :count => 20)
+    @@client.user_timeline(username, :count => 5)
   end
 
   def self.status(tweeet_id)
     tweet = {}
     tweet['text'] = @@client.status(tweeet_id).text
     tweet['created_at'] = @@client.status(tweeet_id).created_at
+    tweet['url'] = @@client.status(tweeet_id).url.to_s
     tweet
   end
 
